e91a5363f6147c938a3c68bd572d9c2731946072
robloxscripts <132229813+spacexroblox@users.noreply.github.com>
2024-06-06 22:15:07 +0200

    Update adminfirst.lua

--- adminfirst.lua
+++ adminfirst.lua
@@ -63,14 +63,33 @@ local Slider = MainTab:CreateSlider({
    end,
 })
 
-local Slider = MainTab:CreateSlider({
-   Name = "JumpPower",
-   Range = {0, 5000},
-   Increment = 1,
-   Suffix = nil,
-   CurrentValue = 50,
-   Flag = "Speed", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-      game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
+local Button = Tab:CreateButton({
+   Name = "Button Example",
+   Interact = 'Click',
+   Callback = function()
+   --Toggles the infinite jump between on or off on every script run
+_G.infinjump = not _G.infinjump
+
+if _G.infinJumpStarted == nil then
+	--Ensures this only runs once to save resources
+	_G.infinJumpStarted = true
+	
+	--Notifies readiness
+	game.StarterGui:SetCore("SendNotification", {Title="WeAreDevs.net"; Text="The WeAreDevs Infinite Jump exploit is ready!"; Duration=5;})
+
+	--The actual infinite jump
+	local plr = game:GetService('Players').LocalPlayer
+	local m = plr:GetMouse()
+	m.KeyDown:connect(function(k)
+		if _G.infinjump then
+			if k:byte() == 32 then
+			humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
+			humanoid:ChangeState('Jumping')
+			wait()
+			humanoid:ChangeState('Seated')
+			end
+		end
+	end)
+end
    end,
 })
