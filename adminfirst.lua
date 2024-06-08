4f904c583065a6d3cd2adb482b57974ab3445c39
robloxscripts <132229813+spacexroblox@users.noreply.github.com>
2024-06-08 14:50:53 +0200

    Update adminfirst.lua

--- adminfirst.lua
+++ adminfirst.lua
@@ -1,85 +1,52 @@
-local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/ArrayField/main/Source.lua'))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Admin Hub | Universal", HidePremium = false, SaveConfig = true, ConfigFolder = "orionadminhub"})
 
-local Window = ArrayField:CreateWindow({
-   Name = "Admin Hub",
-   LoadingTitle = "Admin Hub",
-   LoadingSubtitle = "by Fede",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = AdminHUB, -- Create a custom folder for your hub/game
-      FileName = "adminz"
-   },
-   Discord = {
-      Enabled = true,
-      Invite = "4uJAyPKc", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Admin Hub | KeySystem",
-      Subtitle = "🤨",
-      Note = "Giving u they key bc, adminkey - is the key",
-      FileName = "zazakey", -- It is recommended to use something unique as other scripts using ArrayField may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
-      Actions = {
-            [1] = {
-                Text = 'Click here to copy the key link <--',
-                OnPress = function()
-                    print('Pressed')
-                end,
-                }
-            },
-      Key = {"adminkey"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local Tab1 = Window:MakeTab({
+	Name = "Player",
+	Icon = "rbxassetid://17745859305",
+	PremiumOnly = false
 })
 
-ArrayField:Notify({
-   Title = "Admin Executed!",
-   Content = "Admin executed, you're good to go",
-   Duration = 5,
-   Image = 17745895217,
-   Actions = { -- Notification Buttons
-      Ignore = {
-         Name = "Okay!",
-         Callback = function()
-         print("The user tapped Okay!")
-      end
-   },
- },
+OrionLib:MakeNotification({
+	Name = "Executed!",
+	Content = "Script executed, you're good to go :)",
+	Image = "rbxassetid://17745895217",
+	Time = 5
 })
 
-local MainTab = Window:CreateTab("Player", 17745859305) -- Title, Image
-
-local Slider = MainTab:CreateSlider({
-   Name = "WalkSpeed",
-   Range = {0, 4500},
-   Increment = 1,
-   Suffix = "Speed",
-   CurrentValue = 16,
-   Flag = "WalkSpeed", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-  game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
-   end,
+Tab1:AddSlider({
+	Name = "Walkspeed",
+	Min = 0,
+	Max = 5000,
+	Default = 16,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "Speed",
+	Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
+		print(Value)
+	end    
 })
 
-local Slider = MainTab:CreateSlider({
-   Name = "JumpPower",
-   Range = {0, 1000},
-   Increment = 1,
-   Suffix = "JP",
-   CurrentValue = 50,
-   Flag = "JumpPower", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-  game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
-   end,
+Tab1:AddSlider({
+	Name = "Jump Power",
+	Min = 0,
+	Max = 5000,
+	Default = 50,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "Power",
+	Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = (Value)
+		print(Value)
+	end    
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Inf Jump",
-   Interact = 'Click',
-   Callback = function()
-      --Toggles the infinite jump between on or off on every script run
+Tab1:AddToggle({
+	Name = "Inf Jump",
+	Default = false,
+	Callback = function(Value)
+        --Toggles the infinite jump between on or off on every script run
 _G.infinjump = not _G.infinjump
 
 if _G.infinJumpStarted == nil then
@@ -103,5 +70,51 @@ if _G.infinJumpStarted == nil then
 		end
 	end)
 end
-   end,
+		print(Value)
+	end    
+})
+
+
+local Tab1 = Window:MakeTab({
+	Name = "Troll",
+	Icon = "rbxassetid://17745895217",
+	PremiumOnly = false
+})
+
+Tab1:AddButton({
+	Name = "Bang Script",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/spacexroblox/fullscripts/main/bang-script.lua"))()
+      		print("Bang Loaded")
+  	end    
+})
+
+Tab1:AddButton({
+	Name = "Infinite Yield Bang",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+      		print("Inf IY Loaded")
+  	end    
+})
+
+Tab1:AddButton({
+	Name = "Fling ALL (only games where u can go on people!)",
+	Callback = function()
+		loadstring(game:HttpGet("https://pastebin.com/raw/zqyDSUWX"))()
+      		print("Fling All Loaded")
+  	end    
+})
+
+local Tab1 = Window:MakeTab({
+	Name = "ESP",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab1:AddButton({
+	Name = "ESP Loader",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/spacexroblox/fullscripts/main/esp.lua"))()
+      		print("ESP Loaded")
+  	end    
 })
