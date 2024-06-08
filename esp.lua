9d17129eea7621eeb3085eaeb79696a100d141d8
robloxscripts <132229813+spacexroblox@users.noreply.github.com>
2024-06-08 14:32:04 +0200

    esp.lua

---
+++ esp.lua
@@ -0,0 +1,14 @@
+local game = game
+local select, pcall, loadstring , warn = select, pcall, loadstring, warn
+
+local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/Original.lua"))))
+
+if not Success then
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/Modules/UWP%20Support.lua"))))
+
+    if not Success then
+        return warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
+    end
+end
+
+return ESP
